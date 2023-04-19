6660aa77c574867f0ad2582dfc4f06033ed27928
Jamila <jamnjeri@gmail.com>
2023-04-19 20:50:58 +0300

    Added unique index to the email & password fields.

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_110620) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
