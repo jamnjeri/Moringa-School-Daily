e90256ac6ed7b6f8f7857e1c4bede0375256cabb
Jamila <jamnjeri@gmail.com>
2023-04-19 14:29:39 +0300

    Updated to accept cookies

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # Enable sessions
+    include ActionController::Cookies
+
+    # make available
+    helper_method 
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 end
