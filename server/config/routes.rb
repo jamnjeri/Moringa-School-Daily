9ea9e760209194d17aee12d370e545f1c260a048
Jamila <jamnjeri@gmail.com>
2023-04-20 15:21:51 +0300

    Defined the necessary profile and article routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -13,4 +13,18 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  # PROFILE
+  resources :profiles, only: [:index, :show, :create, :update, :destroy]
+
+  get "latest", to: "profiles#latest"
+
+  # ARTICLES
+  resources :articles, only: [:index, :show, :create, :update, :destroy]
+
+  # Likes
+  patch "/articles/:id/likes", to: "articles#like"
+
+  # Dislikes
+  patch "/articles/:id/dislikes", to: "articles#dislike"
+
 end
