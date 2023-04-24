528d7e48d35c74f73036aaf013d2f247f2c66604
King <kelkab46@gmail.com>
2023-04-24 07:34:04 +0300

    dashboards

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,9 @@ import SignUp from './components/signup';
 function App() {
   return (
     <div className="App">
-      <Login />
-      <SignUp />
-      <SideNavbarAdmin />
-
+      <SideNavbarAdmin/>
+     <Login/>
+     <SignUp/>
     </div>
   );
 }
