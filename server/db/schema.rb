add205e79040410b6531f280a1507f4c608d4db8
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-04-25 18:23:58 +0300

    added notifications

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_235155) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_125012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,6 +114,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_235155) do
     t.string "email", null: false
     t.string "password_digest", null: false
     t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wishlists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
