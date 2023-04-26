67c8c9854036ffc8c3aead03ecd13b5a0d10a0e2
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-04-26 09:16:57 +0300

    made changes on mailers

--- server/app/mailers/user_mailer.rb
+++ server/app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
         @article = article
         @category = Category.joins(:article_categories).where(article_categories: {article_id: article.id}).first
         mail(to: user.email, subject: "New article in #{@category ? @category.name : 'uncategorized'} category")
-        # mail(to: user.email, subject: "New article in #{article.category.name} category")
+        
         
     end
 end
