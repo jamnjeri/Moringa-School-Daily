869811812993489a9faf319b7cc0c55653a642e8
King <kelkab46@gmail.com>
2023-04-27 12:46:39 +0300

    stashing these changes

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -7,7 +7,9 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const navigate = useNavigate ()
+  const navigate = useNavigate()
+  const user = ''
+  
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -23,7 +25,7 @@ const Login = () => {
     setError(null);
 
     // Perform login request
-    fetch('https://ms-daily.onrender.com//login', {
+    fetch('/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: {
@@ -32,8 +34,12 @@ const Login = () => {
     })
       .then((response) => {
         setIsLoading(false);
-        navigate("/sideNavbarAdmin")
-        
+        if (user.role === "admin") {
+          navigate("/sideNavbarAdmin");
+        } else if (user.role === "technicalwriter") {
+          navigate("/newsfeed");
+        }
+
 
         if (!response.ok) {
           setError('Invalid email or password.');
@@ -115,7 +121,7 @@ const Login = () => {
                       {/* <!--Submit button--> */}
                       <div class="mb-12 pb-1 pt-1 text-center">
                         <button
-                         
+
                           class="mb-3 inline-block w-full rounded-full px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                           type="submit"
                           disabled={isLoading}
@@ -143,7 +149,7 @@ const Login = () => {
                           style={{
                             background: ' #F9500D'
                           }}>
-                            Register
+                          Register
                         </Link>
                       </div>
                     </form>
