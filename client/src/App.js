a09e46e61370375fac2a3f14112106d07daeedc6
Jamila <jamnjeri@gmail.com>
2023-05-01 20:52:32 +0300

    Encorporated routing to the main pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import Dashboards from "./pages/Dashboards";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={ <LandingPage /> } />
+        <Route path="/login" element={ <Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboards />} />
+        </Route>
+      </Routes>
   );
 }
 
